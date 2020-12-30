d9f9b7ac866e723a98e68f3cf8b81625130b0064
Danielkevin <45960827+Danielkevin@users.noreply.github.com>
2020-12-31 00:38:32 +0700

    Add files via upload

---
+++ animation.js
@@ -0,0 +1,21 @@
+function xFunction(x)
+{
+    x.class.toggle("change");
+    document.getElementById("Dropdown").classList.toggle("show");
+}
+window.onclick = function(event)
+{
+    if(!event.target.matches('.drop'))
+    {
+        var dropdowns = document.getElementByClassName("drop-content");
+        var i;
+        for(i=0;i<dropdowns.length;i++)
+        {
+            var openDrop = dropdowns[i]
+            if(openDrop.classList.contains('show'))
+            {
+                openDrop.classList.remove('show');
+            }
+        }
+    }
+}
